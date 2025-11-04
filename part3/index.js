db69d98691dc16c0fad7cab10b1f104c297aff2d
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-11-04 17:39:31 -0300

    put and delete added

--- part3/index.js
+++ part3/index.js
@@ -7,12 +7,16 @@ import {Phonebook} from './models/persons.js';
 
 
 const app = express()
-app.use(express.json());
 app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms :body'));
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 app.get('/', (req, resp) => {
   resp.send('<h1>Hello World</h1>')
@@ -24,23 +28,25 @@ app.get('/api/persons', (req, resp) => {
   })
 })
 
-app.get('/api/persons/:id', (req, resp) => {
+app.get('/api/persons/:id', (req, resp, next) => {
   const id = req.params.id
   Phonebook.findById(id)
-    .then(person => { resp.json(person)})
-    .catch(err => console.log("Error:", err))
+    .then(person => { 
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }})
+    .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, resp) => {
+app.delete('/api/persons/:id', (req, resp, next) => {
   const id = req.params.id
   Phonebook.findByIdAndDelete(id)
     .then(() => {
       resp.status(204).end()
     })
-    .catch(err => {
-      console.error('Error deleting person:', err.message)
-      resp.status(400).json({ error: 'malformatted id' })
-    })
+    .catch(err => next(err))
 })
 
 app.post('/api/persons', (req, resp) => {
@@ -82,11 +88,35 @@ app.get('/info', (req, resp) => {
   `)
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+app.put('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+  const body = req.body
 
-app.use(unknownEndpoint)
+  if(!body.name || !body.number) {
+    return res.json({message: "Data missed"})
+  }
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Phonebook.findByIdAndUpdate(id, person, {new: true})
+    .then(updatePerson => {
+      res.json(updatePerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(express.json());
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
