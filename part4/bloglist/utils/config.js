36faa0903057d2acacd3729c752da60b92b25602
Sadelto0130 <49660985+Sadelto0130@users.noreply.github.com>
2025-11-06 14:19:35 -0300

    fixed

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.URI_MONGODB;
 const SECRET = process.env.SECRET;
 
 module.exports = {
   MONGODB_URI,
-  PORT,
+  PORT, 
   SECRET,
 };
